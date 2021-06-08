9e3b9631ce5853cdb8a7f85fe22908822ff13a36
asge4900 <48907330+asge4900@users.noreply.github.com>
2021-06-08 13:05:57 +0200

    First commit

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,15 @@
+﻿using RandomListLibrary;
+using System;
+using System.Linq;
+
+ListNumberService service = new();
+
+var mylist = service.GetRandomList(2000);
+
+foreach (var item in mylist)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine();
+Console.WriteLine($"Sum: {mylist.Sum()}");
---
+++ RandomListLibrary/ListNumberService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomListLibrary
+{
+    public class ListNumberService
+    {
+        private static readonly int[] numbers = new[]
+       {
+            111, 122, 133, 144
+        };
+
+        public List<int> GetRandomList(double maxNumber)
+        {
+            if (maxNumber < 1010)
+            {
+                throw new Exception();
+            }
+            List<int> numberlist = new();
+            Random rng = new ();
+            int number = 0;
+            for (int i = 0; i < maxNumber - 500;)
+            {
+                var newItem = numbers.Where(x => !numberlist.Any(y => x == y));
+                if (i >= 1076 && newItem.Any())
+                {
+                    foreach (var item in newItem)
+                    {
+                        number = item;
+                        numberlist.Add(item);
+                    }
+                }
+                else
+                {
+                    number = numbers[rng.Next(numbers.Length)];
+                    numberlist.Add(number);
+                }
+                i += number;
+            }
+            return numberlist;
+        }
+    }
+}
